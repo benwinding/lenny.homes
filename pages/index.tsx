4a640eb6b44b96144388f7c539db1363133a54f0
Ben Winding <hello@benwinding.com>
2021-11-16 09:15:40 +1100

    Better src image

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { LayoutMain } from '../components/LayoutMain';
 const Page: NextPage = () => {
   const primary = '#333'
   const secondary = '#000'
-  const imageSrc = "/lenny.jpg";
+  const imageSrc = "https://lenny.homes/lenny.jpg";
   return <LayoutMain color={{primary, secondary}}>
     <Head>
       <meta property="og:title" content="Lenny" />
