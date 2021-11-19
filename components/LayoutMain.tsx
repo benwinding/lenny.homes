fdcf14cbd84c27b88a9165fd12d0e1c649e6dd8c
Ben Winding <hello@benwinding.com>
2021-11-19 16:15:31 +1100

    Donate pages added

--- components/LayoutMain.tsx
+++ components/LayoutMain.tsx
@@ -5,7 +5,7 @@ export function LayoutMain(props: { children?: any, color: {primary: string, sec
     colorBgLeft: props.color.primary,
     colorBgRight: props.color.secondary,
   }} >
-    <div className="container p-3 pt-10 sm:p-6 mx-auto p-0 md:p-16 lg:px-auto z-10 relative">
+    <div className="container p-3 pt-10 sm:p-6 mx-auto p-0 md:p-16 z-10 relative">
       {props.children}
     </div>
   </BackGround>
@@ -25,9 +25,11 @@ function BackGround(props: { children: any, config: BackGroundConfig }) {
   const bgHorizont = `linear-gradient(90deg, ${bgColor} ${45 - offset}%, ${lineColor} ${45 - offset}%, ${lineColor} ${55 - offset}%, ${bgColor} ${55 - offset}%, ${bgColor} ${20 - offset}%, ${lineColor} ${20 - offset}%, ${lineColor} ${30 - offset}%,${bgColor} ${30 - offset}%)`;
   const backGroundStr = `${bgVertical}, ${bgHorizont}`;
   const clipStr = `inset(0 0 0 1.5em)`;
-  return <div className="fixed inset-0 overflow-y-auto" style={{ backgroundColor: props.config.colorBgLeft }}>
-    {props.children}
+  return <div className="fixed inset-0" style={{ backgroundColor: props.config.colorBgLeft }}>
     <div className="fixed inset-0" style={{ clipPath: clipStr, left: '70%', background: backGroundStr, backgroundSize: '3em 3em' }}>
+    </div>
+    <div className="w-full h-full overflow-y-auto relative">
+      {props.children}
     </div>
   </div>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,125 @@
 import type { NextPage } from 'next'
 import Head from 'next/head';
 import React from 'react';
+import moment from 'moment';
 import { LayoutMain } from '../components/LayoutMain';
 
 const Page: NextPage = () => {
   const primary = '#333'
   const secondary = '#000'
-  const imageSrc = "https://lenny.homes/lenny.jpg";
-  return <LayoutMain color={{primary, secondary}}>
+  const imageSrc = "/lenny.jpg";
+  const Title = <div className="text-center">
+    <h1>Lenny</h1>
+    <h4 className="text-gray-300 italic -mt-3">{'"Homes"'}</h4>
+  </div>;
+
+  const donateLinks = [
+    {
+      href: '',
+      imageUrl: '/donate_buttons/metamask.png'
+    },
+    {
+      href: '',
+      imageUrl: '/donate_buttons/patreon.png'
+    },
+    {
+      href: '',
+      imageUrl: '/donate_buttons/paypal.png'
+    },
+  ]
+
+  const Donate = <div>
+    <div className="text-center">
+      <h1 className="font-bold">Donate</h1>
+      <p className="text-xl">Do you like to <i>not</i> work? Met too! I{'\''}m looking to travel the world and not work, if you want to help fund my journey, please donate!</p>
+    </div>
+
+    <div className="my-3 flex gap-2 justify-center items-center">
+      {donateLinks.map(({ href, imageUrl }) => <a key={href + imageUrl} href={href}>
+        <img src={imageUrl} width={130} />
+      </a>)}
+    </div>
+
+    <div className="text-center">
+      <p className="text-xl">Remember every one of your dollars will help me get the best content to you! 😁</p>
+      <DonateCount />
+      <p className="mb-5 -mt-3 text-center font-bold">Raised so far!</p>
+    </div>
+  </div>
+
+  return <LayoutMain color={{ primary, secondary }}>
     <Head>
       <meta property="og:title" content="Lenny" />
       <meta property="og:description" content="Homes" />
       <meta property="og:image" content={imageSrc} />
     </Head>
-    <div className="flex flex-col items-center">
-    <h1 className="text-white">Lenny</h1>
-    <h4 className="text-gray-300 italic">{'"Homes"'}</h4>
-    <img src={imageSrc} />
-    </div>
+    <div className="flex flex-col items-center text-white">
+      {Title}
+      {Donate}
+      <img src={imageSrc} width="100%" />
+      <Reviews />
+    </div>    
   </LayoutMain>
 }
 
 export default Page
+
+function DonateCount() {
+  const [count, setCount] = React.useState<string>();
+  React.useEffect(() => {
+    const now = new Date();
+    const countStart = 3000;
+    const countAddedSince = Date2Dollars(now, 12.9);
+    const countWithCommas = (countStart + countAddedSince).toLocaleString();
+    setCount(countWithCommas);
+  }, []);
+  return <h1 className="pr-3">${count}</h1>
+}
+
+function Date2Dollars(endDate: Date, perHour: number): number {
+  const m = moment('2021-11-17', 'YYYY-MM-DD');
+  const diff = -m.diff(endDate, 'hours');
+  return Math.round(diff * perHour);
+}
+
+type Review = {
+  user: string,
+  location: string,
+  comment: string,
+  created_at: string,
+}
+
+function MakeReviewObj(user: string, location: string, comment: string, dateStr: string): Review {
+  return {
+    user,
+    location,
+    comment,
+    created_at: moment(dateStr, 'YYYY-MM-DD').format('LL'),
+  }
+}
+
+function Reviews() {
+  const reviews = [
+    MakeReviewObj('Dave Serin', 'Whyalla, SA', 'I worked hard for this money, but you deserve it more, have a good one!', '2021-10-16'),
+    MakeReviewObj('Michael', 'Whyalla, SA', 'You\'re doing important things, a few bucks is the least I can do for ya! :)', '2021-10-26'),
+    MakeReviewObj('Daniel Roberts', 'Adelaide, SA', 'Thanks for everything you do Guru, enjoy the ride bruzzy 🤙', '2021-10-27'),
+    MakeReviewObj('Annonymous', '-, SA', 'WTF is this? Are you really asking people to give you money to go on holiday!!? Disgusting', '2021-10-28'),
+    MakeReviewObj('Lenny Fan', 'Robe, SA', 'Don\'t let the haters get you down Lou, keep doing what you do bro! 👌👌👌', '2021-10-29'),
+  ];
+
+  return <div className="w-full flex flex-col gap-2">
+    <h1>Reviews</h1>
+    {reviews.map((review) => <ReviewItem key={review.user + review.location} review={review} />)}
+  </div>
+}
+
+function ReviewItem({review}: {review: Review}) {
+  return <div className="text-black rounded-xl bg-gray-200 p-3">
+    <span className="flex items-center gap-2">
+      <p className="font-bold">{review.user}</p>
+      <span className="flex-grow">({review.location})</span>
+      <h5 className="text-xs text-gray-700">{review.created_at}</h5>
+    </span>
+    <p>{review.comment}</p>
+  </div>
+}
