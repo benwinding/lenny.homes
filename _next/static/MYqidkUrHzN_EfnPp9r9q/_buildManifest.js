cbb6ed78ccec74914f2794b6316196f61822ac51
Ben Winding <hello@benwinding.com>
2021-11-19 21:23:38 +1100

    Updates

---
+++ _next/static/MYqidkUrHzN_EfnPp9r9q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-4d2f0a9f494a9dad.js","static/chunks/pages/index-5c51a08716525c0b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
